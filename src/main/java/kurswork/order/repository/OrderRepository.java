3a03bbe474f892eb3930c1bbf7767bd3a649058d
MargaritaKorneichuk <95484537+MargaritaKorneichuk@users.noreply.github.com>
2023-01-19 05:12:09 +0300

    Delete src directory

--- src/main/java/kurswork/order/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package kurswork.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kurswork.order.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
